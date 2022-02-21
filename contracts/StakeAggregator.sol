7690c54c741bae89a7f040f2d080ca1915a00ffe
RuslanMirov <ruslan777mirov@gmail.com>
2022-02-21 11:10:58 +0200

    Update

--- contracts/StakeAggregator.sol
+++ contracts/StakeAggregator.sol
@@ -1,6 +1,18 @@
-contract StakeAggregator {
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract StakeAggregator is Ownable {
   uint256 public totalShares;
+  address[] public assets;
+  address[] public stakes;
+  address public usd;
+
   mapping(address => uint256) public sharesOf;
+  mapping(address => bool) public permittedAsset;
+  mapping(address => bool) public permittedStake;
+
+  constructor(address _usd) public {
+    usd = _usd;
+  }
 
   function getValueInUSD(address asset, uint256 amount)
     public
@@ -10,15 +22,29 @@ contract StakeAggregator {
 
   }
 
+  // User functions
+
   function deposit(address asset, uint256 amount) external {
 
   }
 
-  function withdraw(address asset, uint256 amount) external {
+  function withdraw(uint256 share) external {
 
   }
 
-  function farm(address asset, uint256 amount) external {
-    
+  function claimAndRestake(address stakeFrom, address stakeTo) external {
+
+  }
+
+  // OnlyOwner functions
+
+  function addAsset(address asset) external onlyOwner {
+    assets.push(asset);
+    permittedAsset[asset] = true;
+  }
+
+  function addStake(address stake) external onlyOwner {
+    stakes.push(stake);
+    permittedStake[stake] = true;
   }
 }
