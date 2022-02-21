3aa8cdb2486442b6bc1db83e8f763b7d5650773c
RuslanMirov <ruslan777mirov@gmail.com>
2022-02-21 10:30:22 +0200

    Update

--- contracts/StakeAggregator.sol
+++ contracts/StakeAggregator.sol
@@ -1,3 +1,24 @@
 contract StakeAggregator {
+  uint256 public totalShares;
+  mapping(address => uint256) public sharesOf;
 
+  function getValueInUSD(address asset, uint256 amount)
+    public
+    view
+    returns(uint256)
+  {
+
+  }
+
+  function deposit(address asset, uint256 amount) external {
+
+  }
+
+  function withdraw(address asset, uint256 amount) external {
+
+  }
+
+  function farm(address asset, uint256 amount) external {
+    
+  }
 }
