3ef0f39ff6fdedd0b26be706fee9dc7d93e08892
RuslanMirov <ruslan777mirov@gmail.com>
2022-02-22 11:32:16 +0200

    Replace aggregator with fund

---
+++ contracts/StakeFund.sol
@@ -0,0 +1,484 @@
+pragma solidity ^0.6.12;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
+import "./interfaces/IStake.sol";
+
+
+contract StakeFund is Ownable, IERC20 {
+  using SafeMath for uint256;
+  using SafeERC20 for IERC20;
+
+  // Total amount of ether or stable deposited by all users
+  uint256 public totalWeiDeposited = 0;
+
+  // Total amount of ether or stable withdrawn by all users
+  uint256 public totalWeiWithdrawn = 0;
+
+  // For ERC20 compliance
+  string public name;
+
+  // Percentages are rounded to 3 decimal places
+  uint256 public TOTAL_PERCENTAGE = 10000;
+
+  // Address of the platform that takes a cut from the fund manager success cut
+  address public platformAddress;
+
+  // The percentage of earnings paid to the fund manager. 10000 = 100%
+  // e.g. 10% is 1000
+  uint256 public successFee;
+
+  // The percentage of fund manager earnings paid to the platform. 10000 = 100%
+  // e.g. 10% is 1000
+  uint256 public platformFee;
+
+  // An array of all the erc20 token addresses the smart fund holds
+  address[] public tokenAddresses;
+
+  // the total number of shares in the fund
+  uint256 public totalShares = 0;
+
+  // Denomination of initial shares
+  uint256 constant internal INITIAL_SHARES = 10 ** 18;
+
+  // The earnings the fund manager has already cashed out
+  uint256 public fundManagerCashedOut = 0;
+
+  // Protect from flash loan atack
+  uint256 public fundManagerWithdrawDelay;
+
+  address public coreFundAsset;
+
+  address public stakeAddress;
+
+  // how many shares belong to each address
+  mapping (address => uint256) public addressToShares;
+
+  // this is really only being used to more easily show profits, but may not be necessary
+  // if we do a lot of this offchain using events to track everything
+  // total `depositToken` deposited - total `depositToken` withdrawn
+  mapping (address => int256) public addressesNetDeposit;
+
+  // owner can add/remove swapper
+  mapping (address => bool) public swappers;
+
+
+  event Deposit(address indexed user, uint256 amount, uint256 sharesReceived, uint256 totalShares);
+  event Withdraw(address indexed user, uint256 sharesRemoved, uint256 totalShares);
+
+
+  constructor(
+    address _owner,
+    string memory _name,
+    uint256 _successFee,
+    address _coreFundAsset,
+    address _stakeAddress,
+    address _platformAddress
+  )public{
+    // never allow a 100% fee
+    require(_successFee < TOTAL_PERCENTAGE, "100% fee");
+    require(_owner != address(0), "owner 0x");
+
+    name = _name;
+    successFee = _successFee;
+    platformFee = _successFee; // platform fee the same as manager fee
+    coreFundAsset = _coreFundAsset;
+    stakeAddress = _stakeAddress;
+
+    // Init owner
+    transferOwnership(_owner);
+
+    // Init platform address
+    if(_platformAddress == address(0)){
+      platformAddress = msg.sender;
+    }
+    else{
+      platformAddress = _platformAddress;
+    }
+  }
+
+  /**
+  * @dev Deposits core coin into the fund and allocates a number of shares to the sender
+  * depending on the current number of shares, the funds value, and amount deposited
+  *
+  * @return The amount of shares allocated to the depositor
+  */
+  function deposit(uint256 depositAmount) external returns (uint256) {
+    // Require that the amount sent is not 0
+    require(depositAmount > 0, "ZERO_DEPOSIT");
+
+    // Transfer core ERC20 coin from sender
+    IERC20(coreFundAsset).safeTransferFrom(msg.sender, address(this), depositAmount)
+
+    totalWeiDeposited += depositAmount;
+
+    // Calculate number of shares
+    uint256 shares = calculateDepositToShares(depositAmount);
+
+    // If user would receive 0 shares, don't continue with deposit
+    require(shares != 0, "ZERO_SHARES");
+
+    // Add shares to total
+    totalShares = totalShares.add(shares);
+
+    // Add shares to address
+    addressToShares[msg.sender] = addressToShares[msg.sender].add(shares);
+
+    addressesNetDeposit[msg.sender] += int256(depositAmount);
+
+    emit Deposit(msg.sender, depositAmount, shares, totalShares);
+
+    // stake deposited
+    IERC20(coreFundAsset).approve(stakeAddress, depositAmount);
+    IStake(stakeAddress).stake(depositAmount);
+
+    return shares;
+  }
+
+  function calculateFundValue() public virtual view returns (uint256){
+    return IStake(stakeAddress).balanceOf(address(this))
+           .add(IERC20(coreFundAsset).balanceOf(address(this)));
+  }
+
+  /**
+  * @dev Sends (_mul/_div) of every token (and ether) the funds holds to _withdrawAddress
+  *
+  * @param _mul                The numerator
+  * @param _div                The denominator
+  * @param _withdrawAddress    Address to send the tokens/ether to
+  *
+  * NOTE: _withdrawAddress changed from address to address[] arrays because balance calculation should be performed
+  * once for all usesr who wants to withdraw from the current balance.
+  *
+  */
+  function _withdraw(
+    uint256[] memory _mul,
+    uint256[] memory _div,
+    address[] memory _withdrawAddress
+    )
+    internal
+    returns (uint256)
+  {
+    // Calcualte stake share by user share
+    // Get withdraw amount share by share
+    //
+    // Transfer that _mul/_div of each token we hold to the user
+    uint256 totalStaked = IStake(stakeAddress).balanceOf(address(this))
+
+    // Transfer ERC20 to _withdrawAddress
+    for(uint8 j = 0; j < _withdrawAddress.length; j++){
+      // calculate withdraw ERC20 share
+      uint256 payoutAmount = fundAmount.mul(_mul[j]).div(_div[j]);
+      if(payoutAmount > 0)
+        token.transfer(_withdrawAddress[j], payoutAmount);
+    }
+  }
+
+  /**
+  * @dev Withdraws users fund holdings, sends (userShares/totalShares) of every held token
+  * to msg.sender, defaults to 100% of users shares.
+  *
+  * @param _percentageWithdraw    The percentage of the users shares to withdraw.
+  */
+  function withdraw(uint256 _percentageWithdraw) external {
+    // cache global variables for a save gas
+    uint256 CACHE_TOTAL_PERCENTAGE = TOTAL_PERCENTAGE;
+
+    require(totalShares != 0, "EMPTY_SHARES");
+    require(_percentageWithdraw <= CACHE_TOTAL_PERCENTAGE, "INCORRECT_PERCENT");
+
+    uint256 percentageWithdraw = (_percentageWithdraw == 0) ? CACHE_TOTAL_PERCENTAGE : _percentageWithdraw;
+
+    uint256 addressShares = addressToShares[msg.sender];
+
+    uint256 numberOfWithdrawShares = addressShares.mul(percentageWithdraw).div(CACHE_TOTAL_PERCENTAGE);
+
+    uint256 fundManagerCut;
+    uint256 fundValue;
+
+    // Withdraw the users share minus the fund manager's success fee
+    (fundManagerCut, fundValue, ) = calculateFundManagerCut();
+
+    uint256 withdrawShares = numberOfWithdrawShares.mul(fundValue.sub(fundManagerCut)).div(fundValue);
+
+    // prepare call data for _withdarw
+    address[] memory spenders = new address[](1);
+    spenders[0] = msg.sender;
+
+    uint256[] memory value = new uint256[](1);
+    value[0] = totalShares;
+
+    uint256[] memory cut = new uint256[](1);
+    cut[0] = withdrawShares;
+
+    // do withdraw
+    _withdraw(cut, value, spenders);
+
+    // Store the value we are withdrawing in ether
+    uint256 valueWithdrawn = fundValue.mul(withdrawShares).div(totalShares);
+
+    totalWeiWithdrawn = totalWeiWithdrawn.add(valueWithdrawn);
+    addressesNetDeposit[msg.sender] -= int256(valueWithdrawn);
+
+    // Subtract from total shares the number of withdrawn shares
+    totalShares = totalShares.sub(numberOfWithdrawShares);
+    addressToShares[msg.sender] = addressToShares[msg.sender].sub(numberOfWithdrawShares);
+
+    emit Withdraw(msg.sender, numberOfWithdrawShares, totalShares);
+  }
+
+  /**
+  * @dev Calculates the funds profit
+  *
+  * @return The funds profit in deposit token (Ether)
+  */
+  function calculateFundProfit() public view returns (int256) {
+    uint256 fundValue = calculateFundValue();
+
+    return int256(fundValue) + int256(totalWeiWithdrawn) - int256(totalWeiDeposited);
+  }
+
+  /**
+  * @dev Calculates the amount of shares received according to ether deposited
+  *
+  * @param _amount    Amount of ether to convert to shares
+  *
+  * @return Amount of shares to be received
+  */
+  function calculateDepositToShares(uint256 _amount) public view returns (uint256) {
+    uint256 fundManagerCut;
+    uint256 fundValue;
+
+    // If there are no shares in the contract, whoever deposits owns 100% of the fund
+    // we will set this to 10^18 shares, but this could be any amount
+    if (totalShares == 0)
+      return INITIAL_SHARES;
+
+    (fundManagerCut, fundValue, ) = calculateFundManagerCut();
+
+    uint256 fundValueBeforeDeposit = fundValue.sub(_amount).sub(fundManagerCut);
+
+    if (fundValueBeforeDeposit == 0)
+      return 0;
+
+    return _amount.mul(totalShares).div(fundValueBeforeDeposit);
+
+  }
+
+
+  /**
+  * @dev Calculates the fund managers cut, depending on the funds profit and success fee
+  *
+  * @return fundManagerRemainingCut    The fund managers cut that they have left to withdraw
+  * @return fundValue                  The funds current value
+  * @return fundManagerTotalCut        The fund managers total cut of the profits until now
+  */
+  function calculateFundManagerCut() public view returns (
+    uint256 fundManagerRemainingCut, // fm's cut of the profits that has yet to be cashed out (in `depositToken`)
+    uint256 fundValue, // total value of fund (in `depositToken`)
+    uint256 fundManagerTotalCut // fm's total cut of the profits (in `depositToken`)
+  ) {
+    fundValue = calculateFundValue();
+    // The total amount of ether currently deposited into the fund, takes into account the total ether
+    // withdrawn by investors as well as ether withdrawn by the fund manager
+    // NOTE: value can be negative if the manager performs well and investors withdraw more
+    // ether than they deposited
+    int256 curtotalWeiDeposited = int256(totalWeiDeposited) - int256(totalWeiWithdrawn.add(fundManagerCashedOut));
+
+    // If profit < 0, the fund managers totalCut and remainingCut are 0
+    if (int256(fundValue) <= curtotalWeiDeposited) {
+      fundManagerTotalCut = 0;
+      fundManagerRemainingCut = 0;
+    } else {
+      // calculate profit. profit = current fund value - total deposited + total withdrawn + total withdrawn by fm
+      uint256 profit = uint256(int256(fundValue) - curtotalWeiDeposited);
+      // remove the money already taken by the fund manager and take percentage
+      fundManagerTotalCut = profit.mul(successFee).div(TOTAL_PERCENTAGE);
+      // If manager alredy cut from the best profit period, just return 0
+      fundManagerRemainingCut = fundManagerTotalCut > fundManagerCashedOut
+      ? fundManagerTotalCut.sub(fundManagerCashedOut)
+      : 0;
+    }
+  }
+
+  /**
+  * @dev Allows the fund manager to withdraw their cut of the funds profit
+  */
+  function fundManagerWithdraw() public onlyOwner {
+    require(now >= fundManagerWithdrawDelay, "Need wait 30 seconds after trade");
+
+    uint256 fundManagerCut;
+    uint256 fundValue;
+
+    (fundManagerCut, fundValue, ) = calculateFundManagerCut();
+
+    uint256 platformCut = (platformFee == 0) ? 0 : fundManagerCut.mul(platformFee).div(TOTAL_PERCENTAGE);
+
+    // prepare call data for _withdarw
+    address[] memory spenders = new address[](2);
+    spenders[0] = platformAddress;
+    spenders[1] = owner();
+
+    uint256[] memory value = new uint256[](2);
+    value[0] = fundValue;
+    value[1] = fundValue;
+
+    uint256[] memory cut = new uint256[](2);
+    cut[0] = platformCut;
+    cut[1] = fundManagerCut - platformCut;
+
+    // do withdraw
+    _withdraw(cut, value, spenders);
+
+    // add report
+    fundManagerCashedOut = fundManagerCashedOut.add(fundManagerCut);
+  }
+
+  // calculate the current value of an address's shares in the fund
+  function calculateAddressValue(address _address) public view returns (uint256) {
+    if (totalShares == 0)
+      return 0;
+
+    return calculateFundValue().mul(addressToShares[_address]).div(totalShares);
+  }
+
+  // calculate the net profit/loss for an address in this fund
+  function calculateAddressProfit(address _address) public view returns (int256) {
+    uint256 currentAddressValue = calculateAddressValue(_address);
+
+    return int256(currentAddressValue) - addressesNetDeposit[_address];
+  }
+
+  /**
+  * @dev Allow owner update fund name
+  *
+  * @param _name      new fund name
+  */
+  function updateFundName(string memory _name) external onlyOwner {
+    name = _name;
+  }
+
+  /**
+  * @dev Approve 0 for a certain address
+  *
+  * NOTE: Some ERC20 has no standard approve logic, and not allow do new approve
+  * if alredy approved.
+  *
+  * @param _token                   address of ERC20
+  * @param _spender                 address of spender
+  */
+  function resetApprove(address _token, address _spender) external onlyOwner {
+    IERC20(_token).approve(_spender, 0);
+  }
+
+  // Fallback payable function in order to be able to receive ether from other contracts
+  fallback() external payable {}
+
+  /**
+    **************************** ERC20 Compliance ****************************
+  **/
+
+  // Note that addressesNetDeposit does not get updated when transferring shares, since
+  // this is used for updating off-chain data it doesn't affect the smart contract logic,
+  // but is an issue that currently exists
+
+  event Transfer(address indexed from, address indexed to, uint256 value);
+
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+
+  uint8 public decimals = 18;
+
+  string public symbol = "FND";
+
+  mapping (address => mapping (address => uint256)) internal allowed;
+
+  /**
+  * @dev Total number of shares in existence
+  */
+  function totalSupply() external override view returns (uint256) {
+    return totalShares;
+  }
+
+  /**
+  * @dev Gets the balance of the specified address.
+  *
+  * @param _who    The address to query the the balance of.
+  *
+  * @return A uint256 representing the amount owned by the passed address.
+  */
+  function balanceOf(address _who) external override view returns (uint256) {
+    return addressToShares[_who];
+  }
+
+  /**
+  * @dev Transfer shares for a specified address
+  *
+  * @param _to       The address to transfer to.
+  * @param _value    The amount to be transferred.
+  *
+  * @return true upon success
+  */
+  function transfer(address _to, uint256 _value) external override returns (bool) {
+    require(_to != address(0));
+    require(_value <= addressToShares[msg.sender]);
+
+    addressToShares[msg.sender] = addressToShares[msg.sender].sub(_value);
+    addressToShares[_to] = addressToShares[_to].add(_value);
+    emit Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  /**
+   * @dev Transfer shares from one address to another
+   *
+   * @param _from     The address which you want to send tokens from
+   * @param _to       The address which you want to transfer to
+   * @param _value    The amount of shares to be transferred
+   *
+   * @return true upon success
+   */
+  function transferFrom(address _from, address _to, uint256 _value) external override returns (bool) {
+    require(_to != address(0));
+    require(_value <= addressToShares[_from]);
+    require(_value <= allowed[_from][msg.sender]);
+
+    addressToShares[_from] = addressToShares[_from].sub(_value);
+    addressToShares[_to] = addressToShares[_to].add(_value);
+    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+    emit Transfer(_from, _to, _value);
+    return true;
+  }
+
+  /**
+   * @dev Approve the passed address to spend the specified amount of shares on behalf of msg.sender.
+   * Beware that changing an allowance with this method brings the risk that someone may use both the old
+   * and the new allowance by unfortunate transaction ordering. One possible solution to mitigate this
+   * race condition is to first reduce the spender's allowance to 0 and set the desired value afterwards:
+   * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+   *
+   * @param _spender    The address which will spend the funds.
+   * @param _value      The amount of shares to be spent.
+   *
+   * @return true upon success
+   */
+  function approve(address _spender, uint256 _value) external override returns (bool) {
+    allowed[msg.sender][_spender] = _value;
+    emit Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  /**
+   * @dev Function to check the amount of shares that an owner allowed to a spender.
+   *
+   * @param _owner      The address which owns the funds.
+   * @param _spender    The address which will spend the funds.
+   *
+   * @return A uint256 specifying the amount of shares still available for the spender.
+   */
+  function allowance(address _owner, address _spender) external override view returns (uint256) {
+    return allowed[_owner][_spender];
+  }
+}
--- contracts/interfaces/IStake.sol
+++ contracts/interfaces/IStake.sol
@@ -1,7 +1,5 @@
 interface IStake {
-  function stakeFor(uint256 amount, address forAddress) external;
+  function stake(uint256 amount) external;
   function withdraw(uint256 amount) external;
-  function getReward() external;
-  function rewardsToken() external view returns(address);
-  function earnedByShare(uint256 share) external view returns (uint256);
+  function earned(address account) public view returns (uint256);
 }
